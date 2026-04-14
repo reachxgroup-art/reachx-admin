1855f969512c52c956e3a11c92d85c414dbc7221
reachxgroup-art <reachxgroup@gmail.com>
2026-04-15 03:59:15 +0530

    Update api.jsx

--- frontend/frontend/src/api/api.jsx
+++ frontend/frontend/src/api/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://reachx-admin-1.onrender.com/";
+const BASE_URL = "https://reachx-admin-1.onrender.com";
 
 const api = axios.create({ baseURL: BASE_URL });
 
